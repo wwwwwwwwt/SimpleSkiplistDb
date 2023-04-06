9a803af8161675fbbf90e1d2fabb96c0c6e6aba7
wwwwwwwwt <1162933894@qq.com>
2023-04-06 23:11:30 +0800

    to do client login

--- myRedis_server/include/my_thread.h
+++ myRedis_server/include/my_thread.h
@@ -2,7 +2,7 @@
  * @Author: zzzzztw
  * @Date: 2023-03-21 19:45:26
  * @LastEditors: Do not edit
- * @LastEditTime: 2023-04-06 22:43:11
+ * @LastEditTime: 2023-04-06 23:11:08
  * @FilePath: /SimpleSkiplistDB/myRedis_server/include/my_thread.h
  */
 #include <thread>
@@ -16,10 +16,6 @@ public:
 
     my_thread(my_thread && other):thread_(std::move(other.thread_)){};
 
-    my_thread(const my_thread & other) = delete;
-    /*void detach(){
-        thread_.detach();
-    }*/
     ~my_thread(){
         if(thread_.joinable()){
             thread_.join();
